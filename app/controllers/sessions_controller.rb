568fb13042521b288cea998d3d6f2167778aa839
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-10-27 22:27:04 -0400

    reset heroku blew up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to(session[:intended_url] || user)
+      redirect_to(session[:intended_url] || products_path)
       session[:intended_url] = nil
     else
       flash.now[:alert] = "Invalid email/password combination!"
