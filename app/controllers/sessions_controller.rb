ee069631f02db448fd69a5567ba231bf9f61506c
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-11-18 10:21:01 -0500

    styling index again (#2)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      redirect_to(session[:intended_url] || products_path)
+      redirect_to(session[:intended_url] || filtered_products_path(:trending))
       session[:intended_url] = nil
     elsif 
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to(session[:intended_url] || products_path)
+    redirect_to(session[:intended_url] || filtered_products_path(:trending))
     session[:intended_url] = nil
     else  
       flash.now[:alert] = "Invalid email/password combination!"
