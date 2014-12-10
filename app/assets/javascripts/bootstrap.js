95ef812776f3419d5e45f23fc7a60b922679c6f0
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-12-09 20:07:33 -0500

    tweetz

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2098,6 +2098,9 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
     return this
   }
 
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+twttr.widgets.load();
+
 
   // AFFIX DATA-API
   // ==============
