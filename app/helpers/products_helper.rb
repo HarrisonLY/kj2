2f351aa444fd148e19286a3882743474a7c80a60
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2014-12-20 23:24:36 -0500

    tutorial Vid

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -37,7 +37,11 @@ def image_for(product)
 end
 
 
-
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+  
 
 # images for product
 
